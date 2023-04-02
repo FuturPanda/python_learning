62e71cf71645b7331e740f6bebb07e1ddd5363eb
Futur - Panda <sty.hoareau@gmail.com>
2023-04-02 22:02:54 +0200

    jupyter notebooks

--- Andrew Ng - Machine Learning Coursera/linear_regression_one_var.py
+++ Andrew Ng - Machine Learning Coursera/linear_regression_one_var.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use('./deeplearning.mplstyle')
+# plt.style.use('./deeplearning.mplstyle')
 
 # x_train is the input variable (size in 1000 square feet)
 # y_train is the target (price in 1000s of dollars)
